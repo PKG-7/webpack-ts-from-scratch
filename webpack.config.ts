4f39151ffee619d386b45a48e3fa2d4f1246f68c
PKG-7 <pavel.k.group@gmail.com>
2024-03-07 07:37:18 +0300

    dev server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,16 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "path";
 import webpack from "webpack";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 interface EnvVariables {
   mode: "development" | "production";
+  port: 3013;
 }
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === "development";
+
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
     entry: path.resolve(__dirname, "src", "index.ts"),
@@ -24,14 +28,21 @@ export default (env: EnvVariables) => {
         },
       ],
     },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "public", "index.html"),
       }),
     ],
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    devtool: isDev && "inline-source-map",
+    devServer: isDev
+      ? {
+          port: env.port ?? 3000,
+          open: true,
+        }
+      : undefined,
   };
   return config;
 };
