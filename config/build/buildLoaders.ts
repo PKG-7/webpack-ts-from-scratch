805204b173024f9ce1cac02503874e9ea3a938d8
PKG-7 <pavel.k.group@gmail.com>
2024-03-10 04:18:14 +0300

    scss modules with naming

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,13 +3,24 @@ import { ModuleOptions } from "webpack";
 import { BuildOptions } from "./types/types";
 
 export function buildLoaders({ mode }: BuildOptions): ModuleOptions["rules"] {
+  //Тут можно задать формат стилей
+  //https://webpack.js.org/loaders/css-loader/#localidentname
+  const scssLoaderWithModules = {
+    loader: "css-loader",
+    options: {
+      modules: {
+        localIdentName: "[local]__[hash:base64:5]",
+      },
+    },
+  };
+
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
       // Creates 'style' nodes from JS strings
       mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
       //Translates CSS in common JS
-      "css-loader",
+      scssLoaderWithModules,
       // Compiles Sass to CSS
       "sass-loader",
       ,
