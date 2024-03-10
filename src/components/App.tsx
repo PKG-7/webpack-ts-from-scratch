d304a458e9096a59df0258e9dacd195c3ba506ed
PKG-7 <pavel.k.group@gmail.com>
2024-03-10 11:16:46 +0300

    feat: styling main page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
 import st from "./App.module.scss";
+import { Outlet } from "react-router-dom";
+import { SiWebpack } from "react-icons/si";
 
 export default function App() {
   const [count, setCount] = useState(0);
+
   return (
     <main className={st.main}>
-      <h1 className={st.header}> classNameHello, world!</h1>
-      <p>You clicked {count} times</p>
+      <h1 className={st.header}>
+        <SiWebpack /> Hello, WebPack!
+      </h1>
+      <div className={st.counter}>
+        <p>
+          You clicked
+          <span className={st.count}> {count}</span> times
+        </p>
+      </div>
 
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <div className={st.buttonContainer}>
+        <button className={st.button} onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+      {/* <Outlet /> */}
     </main>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./styles/index.scss";
 
 const root = document.getElementById("root");
 
@@ -9,4 +11,21 @@ if (!root) {
 
 const container = createRoot(root);
 
-container.render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/about",
+        element: <h1>About</h1>,
+      },
+      {
+        path: "/shop",
+        element: <h1>Shop</h1>,
+      },
+    ],
+  },
+]);
+
+container.render(<RouterProvider router={router} />);
