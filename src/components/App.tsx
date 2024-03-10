f4ebf0dcf2e83ff45e27cde31e439b5c7062942b
PKG-7 <pavel.k.group@gmail.com>
2024-03-10 04:02:31 +0300

    scss modules working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import "./App.scss";
+import st from "./App.module.scss";
 
 export default function App() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1> classNameHello, world!</h1>
       <p>You clicked {count} times</p>
+      <div className={st.dik}>asd</div>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
